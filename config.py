764ba4f85fa764af435dfca2bb0fa736710ec235
Salvador Medina <salbed.medina@gmail.com>
2019-07-23 00:04:17 -0400

    Add conda env yaml

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 params = dict()
 
-params['num_classes'] = 101
+params['num_classes'] = 20
 
-params['dataset'] = '/disk/dataset/UCF-101'
+params['dataset'] = '../../Data/VIRAT/slowfast_clips'
 
 params['epoch_num'] = 40
-params['batch_size'] = 16
+params['batch_size'] = 32 
 params['step'] = 10
-params['num_workers'] = 4
+params['num_workers'] = 8
 params['learning_rate'] = 1e-2
 params['momentum'] = 0.9
 params['weight_decay'] = 1e-5
 params['display'] = 10
 params['pretrained'] = None
-params['gpu'] = [0]
+params['gpu'] = [1]
 params['log'] = 'log'
-params['save_path'] = 'UCF101'
+params['save_path'] = 'VIRAT'
 params['clip_len'] = 64
 params['frame_sample_rate'] = 1
 params['patience'] = 5
